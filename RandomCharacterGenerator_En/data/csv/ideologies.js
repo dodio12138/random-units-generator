44815b1a120773ab14633abf2dbffba4d989d9d1
dodio12138 <1159715241@qq.com>
2021-03-31 21:06:25 +0800

    1

---
+++ RandomCharacterGenerator_En/data/csv/ideologies.js
@@ -0,0 +1,522 @@
+ideologies = [
+    {
+        "name": "Anarcho-Communism",
+		"name_zh": "无政府共产主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 9
+        }
+    },
+    {
+        "name": "Libertarian Communism",
+		"name_zh": "自由共产主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 7,
+            "govt": 8,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Trotskyism",
+		"name_zh": "托洛茨基主义",
+        "stats": {
+            "econ": 10
+            "dipl": 10,
+            "govt": 6,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Marxism",
+		"name_zh": "马克思主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 7,
+            "govt": 4,
+            "scty": 8
+        }
+    },
+    {
+        "name": "De Leonism",
+		"name_zh": "德莱昂主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 3,
+            "govt": 3,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Leninism",
+		"name_zh": "列宁主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 4,
+            "govt": 2,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Stalinism/Maoism",
+		"name_zh": "斯大林主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 6
+        }
+    },
+    {
+        "name": "Religious Communism",
+		"name_zh": "宗教共产主义",
+        "stats": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": 3,
+            "scty": 3
+        }
+    },
+    {
+        "name": "State Socialism",
+		"name_zh": "国家社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 3,
+            "govt": 3,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Theocratic Socialism",
+		"name_zh": "神权社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 5,
+            "govt": 3,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Religious Socialism",
+		"name_zh": "宗教社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 5,
+            "govt": 7,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Democratic Socialism",
+		"name_zh": "民主社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 5,
+            "govt": 5,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Revolutionary Socialism",
+		"name_zh": "革命社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 2,
+            "govt": 5,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Libertarian Socialism",
+		"name_zh": "自由意志社会主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 8,
+            "govt": 8,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Anarcho-Syndicalism",
+		"name_zh": "无政府工团主义",
+        "stats": {
+            "econ": 8,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Left-Wing Populism",
+		"name_zh": "左翼民粹主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 4,
+            "govt": 3,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Theocratic Distributism",
+		"name_zh": "神权分配主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 4,
+            "govt": 3,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Distributism",
+		"name_zh": "分配主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 5,
+            "govt": 5,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Social Liberalism",
+		"name_zh": "社会自由主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 6,
+            "govt": 6,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Christian Democracy",
+		"name_zh": "基督教民主主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 6,
+            "govt": 5,
+            "scty": 3
+        }
+    },
+    {
+        "name": "Social Democracy",
+		"name_zh": "社会民主主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 7,
+            "govt": 6,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Progressivism",
+		"name_zh": "进步主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 8,
+            "govt": 6,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Anarcho-Mutualism",
+		"name_zh": "无政府互助主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 7
+        }
+    },
+    {
+        "name": "National Totalitarianism",
+		"name_zh": "国家极权主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Global Totalitarianism",
+		"name_zh": "全球极权主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 8,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Technocracy",
+		"name_zh": "技治主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 6,
+            "govt": 2,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Centrist",
+		"name_zh": "中间主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 5,
+            "govt": 5,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Liberalism",
+		"name_zh": "自由主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 6,
+            "govt": 6,
+            "scty": 6
+        }
+    },
+    {
+        "name": "Religious Anarchism",
+		"name_zh": "宗教无政府主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Right-Wing Populism",
+		"name_zh": "右翼民粹主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 3,
+            "govt": 3,
+            "scty": 3
+        }
+    },
+    {
+        "name": "Moderate Conservatism",
+		"name_zh": "中间保守主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 4,
+            "govt": 5,
+            "scty": 3
+        }
+    },
+    {
+        "name": "Reactionary",
+		"name_zh": "反动主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 4,
+            "govt": 4,
+            "scty": 1
+        }
+    },
+    {
+        "name": "Social Libertarianism",
+		"name_zh": "社会自由意志主义",
+        "stats": {
+            "econ": 6,
+            "dipl": 7,
+            "govt": 8,
+            "scty": 7
+        }
+    },
+    {
+        "name": "Libertarianism",
+		"name_zh": "自由意志主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 6,
+            "govt": 8,
+            "scty": 6
+        }
+    },
+    {
+        "name": "Anarcho-Egoism",
+		"name_zh": "利己主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Nazism",
+		"name_zh": "纳粹主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0.5
+        }
+    },
+    {
+        "name": "Autocracy",
+		"name_zh": "专制主义",
+        "stats": {
+            "econ": 5,
+            "dipl": 2,
+            "govt": 2,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Fascism",
+		"name_zh": "法西斯主义",
+        "stats": {
+            "econ": 4,
+            "dipl": 2,
+            "govt": 2,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Capitalist Fascism",
+		"name_zh": "资本法西斯主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": 2,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Conservatism",
+		"name_zh": "保守主义",
+        "stats": {
+            "econ": 3,
+            "dipl": 4,
+            "govt": 4,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Neo-Liberalism",
+		"name_zh": "新自由意志主义",
+        "stats": {
+            "econ": 3,
+            "dipl": 3,
+            "govt": 5,
+            "scty": 6
+        }
+    },
+    {
+        "name": "Classical Liberalism",
+		"name_zh": "古典自由主义",
+        "stats": {
+            "econ": 3,
+            "dipl": 6,
+            "govt": 6,
+            "scty": 8
+        }
+    },
+    {
+        "name": "Authoritarian Capitalism",
+		"name_zh": "威权资本主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 3,
+            "govt": 2,
+            "scty": 4
+        }
+    },
+    {
+        "name": "State Capitalism",
+		"name_zh": "国家资本主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 5,
+            "govt": 3,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Neo-Conservatism",
+		"name_zh": "新保守主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 2,
+            "govt": 4,
+            "scty": 2
+        }
+    },
+    {
+        "name": "Fundamentalism",
+		"name_zh": "原教旨主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 3,
+            "govt": 3,
+            "scty": 0.5
+        }
+    },
+    {
+        "name": "Libertarian Capitalism",
+		"name_zh": "自由资本主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 5,
+            "govt": 8,
+            "scty": 6
+        }
+    },
+    {
+        "name": "Market Anarchism",
+		"name_zh": "市场无政府主义",
+        "stats": {
+            "econ": 2,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Objectivism",
+		"name_zh": "客观主义",
+        "stats": {
+            "econ": 1,
+            "dipl": 5,
+            "govt": 9,
+            "scty": 4
+        }
+    },
+    {
+        "name": "Totalitarian Capitalism",
+		"name_zh": "极权资本主义",
+        "stats": {
+            "econ": 0,
+            "dipl": 3,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Ultra-Capitalism",
+		"name_zh": "超资本主义",
+        "stats": {
+            "econ": 0,
+            "dipl": 4,
+            "govt": 5,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Anarcho-Capitalism",
+		"name_zh": "无政府资本主义",
+        "stats": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 5
+        }
+    }
+];
