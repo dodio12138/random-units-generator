ab2fabf4afbcedec2039758c58904c480b1e700e
dodio12138 <1159715241@qq.com>
2021-04-01 13:53:22 +0800

    1

--- RandomCharacterGenerator_En/ideologiesLoad.py
+++ RandomCharacterGenerator_En/ideologiesLoad.py
@@ -6,6 +6,14 @@
 @time:2021/03/31
 """
 
-if __name__ == '__main__':
-    pass
-  
+import json
+
+with open('data/csv/ideologies_copy.json', 'r', encoding='utf-8') as ide:
+    data = [{"a": 1, "c": 3, "b": 2, "e": 5, "d": 4}]
+    data_ide = json.load(ide)
+    cor = 0
+    test = [5, 3, 6, 7]
+    for i in range(len(data_ide['ideologies'])):
+        stats = data_ide['ideologies'][i]['stats']
+        
+        print(stats)
